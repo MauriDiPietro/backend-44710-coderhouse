1760dfe3fddbd53af487c10778ddf3a89570c085
MauriDiPietro <dipietro.jm@gmail.com>
2023-04-04 21:12:22 -0300

    clase 03

---
+++ 03 - NUEVAS FUNCIONALIDADES ECMASCRIPT/02-es8.js
@@ -0,0 +1,14 @@
+/* -------------------------------------------------------------------------- */
+/*                               //ECMASCRIPT 8                               */
+/* -------------------------------------------------------------------------- */
+
+const object = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4
+};
+
+console.log('Object.entries: ', Object.entries(object));
+console.log('Object.keys: ', Object.keys(object));
+console.log('Object.values: ', Object.values(object));
