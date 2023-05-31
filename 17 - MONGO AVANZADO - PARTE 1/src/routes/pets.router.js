7c7afab228b30307841d08338872636dad53bb4d
MauriDiPietro <dipietro.jm@gmail.com>
2023-05-30 21:03:17 -0300

    clase 17

---
+++ 17 - MONGO AVANZADO - PARTE 1/src/routes/pets.router.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import * as controller from '../controllers/pets.controller.js';
+
+const router = Router()
+
+router.post('/', controller.createPet);
+router.post('/add/:idUser/:idPet', controller.addPetToUser);
+router.get('/:id', controller.getByIdPet);
+
+export default router;
